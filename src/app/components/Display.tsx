0b62394c3eb84ceda0a2e1e7e3e050c517939e55
Prathamesh Sirdesai <sirdesai.work@gmail.com>
2024-03-04 17:28:50 +0530

    added captureKeys State

--- src/app/components/Display.tsx
+++ src/app/components/Display.tsx
@@ -1,23 +1,43 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 const Display = (props: Props) => {
-  const [key, setKey] = useState("");
+  const [key, setKey] = useState("Press keys...");
+  const [captureKeys, setcaptureKeys] = useState<string[]>([]);
+
   useEffect(() => {
-    function captureKeydown(event: KeyboardEvent){
+    function captureKeydown(event: KeyboardEvent) {
       const keyPressed = event.key;
-      const isAlphabetic = /^[A-Za-z,.]$/.test(keyPressed);
-      if (isAlphabetic) setKey(event.key);
+      const isAlphabetic = /^[A-Za-z,.\s]$/.test(keyPressed);
+      if (isAlphabetic) {
+        setKey(event.key);
+        setcaptureKeys((prevData) => [...prevData, keyPressed]);
+      }
     }
 
-    window.addEventListener("keydown", captureKeydown);
+    if (typeof window !== "undefined") {
+      window.addEventListener("keydown", captureKeydown);
+    }
+
+    // window.addEventListener("keydown", captureKeydown);
+
+    return () => {
+      // Remove the event listener when the component unmounts
+      if (typeof window !== "undefined") {
+        window.removeEventListener("keydown", captureKeydown);
+      }
+    };
+
   }, []);
   return (
-    <div className="p-10 border-2 w-4/5 h-3/5 flex justify-center items-center text-9xl font-semibold rounded-lg shadow-lg bg-sky-100">
-      {key}
-    </div>
+    <>
+      <div className="font-semibold text-3xl">{captureKeys}</div>
+      <div className="p-10 border-2 w-4/5 h-3/5 flex justify-center items-center text-9xl font-semibold rounded-lg shadow-lg bg-sky-100">
+        {key}
+      </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Display from "./components/Display";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center h-screen">
         <Display/>
       </div>
     </>

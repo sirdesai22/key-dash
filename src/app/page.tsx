2bc21e8b9c85415a5abc5cebf95d4e9ed1ff611b
sirdesai22 <sirdesai.work@gmail.com>
2024-05-01 13:51:42 +0530

    routes handled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
   const redirectStats = async(time:number) => {
     try {
-      const response = await fetch("http://localhost:3000/api/wpm", {
+      const response = await fetch("/api/wpm", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
